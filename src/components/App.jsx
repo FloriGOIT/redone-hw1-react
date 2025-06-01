944b88a0098073d75ed9522cce5025d0c2008f2b
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-06-01 16:31:42 +0300

    first setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <h1>Redone - hw1 - react</h1>
+      
+    
 };
