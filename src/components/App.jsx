ccdb92f0ea8472f4dd1be7fe877f2371918f2195
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-06-03 21:30:33 +0300

    Profile income Florentina completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
-import Profile from "./Profile/Profile"
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
+import me from './Profile/WhatsApp Image 2025-06-01 at 20.27.49.jpeg';
+
 
 function App() {
-  
+  const { username, tag, location, stats } = user;
   return (
     <>
       <h1>Redone - hw1 - react</h1>
-      <Profile />
+      <Profile
+        username={username}
+        tag={tag}
+        me={me}
+        location={location}
+        stats={stats}
+      />
     </>
-
-  )
+  );
 }
-export default App
+export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,43 @@
-import style from "./profile.module.sass"
+import style from './profile.module.scss';
+import PropTypes from 'prop-types';
 
-function Profile() {
+
+function Profile({ username, tag, me, location, stats }) {
   return (
     <>
-      <h3 className={style.title}>Profile</h3>
+      <section> 
+        <h2>Achievement</h2>
+        <div className={style.userInfo}>
+          <img src={me} alt={username} width={100} />
+          <h4>{username}</h4>
+          <h5>@{tag}</h5>
+          <h5>{location}</h5>
+        </div>
+        <div className={style.statsInfo}>
+          <div className={style.stats}>
+            <p >Income</p>
+            <span >{stats.income}</span>
+          </div>
+          <div className={style.stats}>
+            <p>Currency</p>
+            <span>{stats.currency}</span>
+          </div>
+          <div className={style.stats}>
+            <p>Frequency</p>
+            <span>{stats.frequency}</span>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
+
+Profile.propTypes= {
+  username: PropTypes.string,
+  location: PropTypes.string,
+  me: PropTypes.string,
+  tag: PropTypes.string,
+  stats:PropTypes.shape({income:PropTypes.number.isRequired, currency: PropTypes.string, frequency:PropTypes.string})
+}
