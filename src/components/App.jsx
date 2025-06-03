46905ba80ff3d15d05abbb2689b52458e9067e47
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-06-03 07:18:29 +0300

    ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
+import Profile from "./Profile/Profile"
+
 function App() {
   
   return (
     <>
       <h1>Redone - hw1 - react</h1>
-      <p>Ana are mere</p>
+      <Profile />
     </>
 
   )
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import style from "./profile.module.sass"
+
+function Profile() {
+  return (
+    <>
+      <h3 className={style.title}>Profile</h3>
+    </>
+  )
+}
+
+export default Profile
