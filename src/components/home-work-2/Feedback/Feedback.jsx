6b4c2c999bb1565833e6f344f8e0a2a6e6b41b43
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-06-06 20:22:24 +0300

    redone Feedback component

--- src/components/home-work-2/Feedback/Feedback.jsx
+++ src/components/home-work-2/Feedback/Feedback.jsx
@@ -1,28 +1,52 @@
 import React from 'react';
 import style from './feedback.module.scss';
 
-   class Feedback extends React.Component {
-      state = { good: 0, bad: 0, neutral: 0 };
-      handleFeedback = (type) => { this.setState(prev => ({ [type]: prev[type] + 1 }));  console.log(this.state)}
-   
-      render() {
-         const options = Object.keys(this.state);
-         let total = this.state.good+this.state.bad+this.state.neutral;
-         let percentage = ((this.state.good / total)*100).toFixed(2)
+class Feedback extends React.Component {
+  state = { good: 0, bad: 0, neutral: 0 };
+  handleFdbk = option =>
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
 
-      return (
-         <div className={style.feedbackAll}>
-         <div className={style.btnsFeedback}>
-               {options.map((type,index) => {return <button type="button" onClick={()=>this.handleFeedback(type)} key={index}>{type}</button>})}
-            </div>
-            <div className={style.statsFeedback}>
-               {options.map(optType => { return <span key={optType}>{optType.toUpperCase()}: {this.state[optType]}</span> })}
-               <span key="total">Total:{total}</span>
-               {total>0 ? <span key="percentage">Positive percentage:{percentage} </span>: null}
-         </div>
-         </div>
-      );
-   }
-   }
+  render() {
+    let total = this.state.good + this.state.bad + this.state.neutral;
+    let percentage = ((this.state.good / total) * 100).toFixed(2);
+    let options = Object.keys(this.state);
+    return (
+      <section className={style.feedbackAll}>
+        <h2>Offer feedback</h2>
+        <br />
+        <div className={style.btns}>
+          {options.map(option => {
+            return (
+              <button
+                type="button"
+                key={option + `Btn`}
+                onClick={() => this.handleFdbk(option)}
+              >
+                {option.toUpperCase()}
+              </button>
+            );
+          })}
+        </div>
+        <h3>Cumulated stats</h3>
+        <div className={style.feedback}>
+          {options.map(option => {
+            return (
+              <span key={option + `Span`}>
+                {option.toUpperCase()}: {this.state[option]}
+              </span>
+            );
+          })}
+          <span>TOTAL: {total}</span>
+          <span style={{ visibility: total > 0 ? `visible` : `hidden` }}>
+            Percentage of positive: {percentage} %
+          </span>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Feedback;
+
+
+
