8693c510f9129f954b0e8e360cb7c28b0b60b6ee
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-06-11 18:16:10 +0300

    <ImgGallery/> in progress

--- src/components/test/services.jsx
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const fetchArticlesWithQuery = async searchQuery=>
-{const infoFetched = await axios.get(`/search?query=${searchQuery}`);
-return infoFetched.data.hits;
-}
