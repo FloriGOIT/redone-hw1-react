e8269cd4b869c02984abd8c08a5754474cda72a8
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-06-12 17:57:51 +0300

    ..okisch

--- src/components/home-work-3/ImgGallery.jsx
+++ src/components/home-work-3/ImgGallery.jsx
@@ -1,46 +1,74 @@
 import style from './imgGallery.module.scss';
 import axios from 'axios';
 import React from 'react';
-import ContentLoader from "react-content-loader";
+import ContentLoader from 'react-content-loader';
 
 //<Searchbar>, <ImgGallery>, <ImageGalleryItem>, <Loader>, <Button> и <Modal>
 
-const fetchImgs = async query => {
-
-  
+const fetchImgs = async (page, query) => {
   const apiB = axios.create({
-    baseURL: `https://pixabay.com/api/?key=42799638-b50871d8c9a958480a9d6ba7c&image_type=photo&orientation=horizontal&per_page=9&page=5`,
+    baseURL: `https://pixabay.com/api/?key=42799638-b50871d8c9a958480a9d6ba7c&image_type=photo&orientation=horizontal`,
   });
-  const imagesInfo = await apiB.get(`&q=${query}`);
+  const imagesInfo = await apiB.get(`&per_page=9&page=${page}&q=${query}`);
   return imagesInfo.data.hits;
 };
 
 class ImgGallery extends React.Component {
-  state = { imagesArr: [], error2: null, isLoading2: false, query: '' };
-  handleQueryChange = (input) => { this.setState({ query: input }); console.log("input", input); console.log("state", this.state.query) }
-  
+  state = {
+    imagesArr: [],
+    error2: null,
+    isLoading2: false,
+    query: '',
+    imageModal: '',
+    imageDescription: '',
+    isOpenModal: false,
+    page:1
+  };
+  handleQueryChange = input => {
+    this.setState({ query: input });
+    console.log('input', input);
+    console.log('state', this.state.query);
+  };
+  handleImg = (input1, input2) =>
+    this.setState({ imageModal: input1, imageDescription: input2, isOpenModal: true });
+  handleCloseModal = () => {
+    this.setState({
+      isOpenModal: false, imageModal: '',
+      imageDescription: ''
+    });
+ console.log("handleCloseModal",this.state)};
   async componentDidUpdate(prevProps, prevState) {
-    if(prevState.query !== this.state.query){
-    this.setState({ isLoading2: true });
-    try {
-      const data = await fetchImgs(`&q=${this.state.query}`);
-      this.setState({ imagesArr: data });
-    } catch (error) {
-      this.setState({ error2: error });
-    } finally {
-      this.setState({ isLoading2: false });
-    }}
+    if (prevState.query !== this.state.query) {
+      this.setState({ isLoading2: true });
+      try {
+        const data = await fetchImgs(this.state.page, this.state.query);
+        this.setState({ imagesArr: data });
+      } catch (error) {
+        this.setState({ error2: error });
+      } finally {
+        this.setState({ isLoading2: false });
+      }
+    }
   }
 
   render() {
-    //console.log('imagesArr', this.state.imagesArr);
+    console.log('imagesArr', this.state);
     return (
       <section className={style.imgGalleryAll}>
-                
-        <Search handleQueryChange={this.handleQueryChange} />
-        <ImageGalleryItem data={this.state.imagesArr} />
-        {this.state.isLoading2 && <Loader/>}
+        <div className={style.imgGallery}>
+          <Search handleQueryChange={this.handleQueryChange} />
+          <ImageGalleryItem
+            data={this.state.imagesArr}
+            handleImg={this.handleImg}
+          />
+          {this.state.isLoading2 && <MyLoader />}
+        </div>
 
+        {this.state.isOpenModal && <Modal
+          imageModal={this.state.imageModal}
+          imageDescription={this.state.imageDescription}
+          handleCloseModal={this.handleCloseModal}
+        />}
       </section>
     );
   }
@@ -49,14 +77,17 @@ export default ImgGallery;
 
 class ImageGalleryItem extends React.Component {
   render() {
-    const { data } = this.props;
-    //console.log('data', data);
+    const { data, handleImg } = this.props;
+
     return (
       <ul className={style.imgGalleryList}>
         {data.map(({ id, tags, webformatURL }) => {
           return (
-            <li key={id}>
-              <img src={webformatURL} alt={tags} />
+            <li key={id} onClick={() => handleImg(webformatURL, tags)}>
+              <img
+                src={webformatURL}
+                alt={tags}
+              />
             </li>
           );
         })}
@@ -66,20 +97,20 @@ class ImageGalleryItem extends React.Component {
 }
 
 class Search extends React.Component {
-  handleInputChange = e => { e.preventDefault(); let input = e.target.elements.inputSch.value;  this.props.handleQueryChange(input)}
+  handleInputChange = e => {
+    e.preventDefault();
+    let input = e.currentTarget.elements.inputSch.value.trim();
+    this.props.handleQueryChange(input);
+  };
   render() {
-   
     return (
       <form className={style.searchImg} onSubmit={this.handleInputChange}>
         <button>🍳</button>
-        <input type="text" name="inputSch" placeholder='Seatch a noun.....' />
+        <input type="text" name="inputSch" placeholder="Search a noun....." />
       </form>
     );
   }
 }
-
-
-
 
 const MyLoader = () => (
   <ContentLoader
@@ -94,13 +125,29 @@ const MyLoader = () => (
     <rect x="20" y="0" rx="5" ry="5" width="10" height="10" />
     <rect x="40" y="0" rx="5" ry="5" width="10" height="10" />
     <rect x="60" y="0" rx="5" ry="5" width="10" height="10" />
-    
+    <rect x="80" y="0" rx="5" ry="5" width="10" height="10" />
+    <rect x="100" y="0" rx="5" ry="5" width="10" height="10" />
+    <rect x="120" y="0" rx="5" ry="5" width="10" height="10" />
+    <rect x="140" y="0" rx="5" ry="5" width="10" height="10" />
+    <rect x="160" y="0" rx="5" ry="5" width="10" height="10" />
   </ContentLoader>
-)
+);
 
-class Loader extends React.Component{
+class Modal extends React.Component {
 
+  handleClose = e => {
+    console.log("e.target",e.target); console.log("e.currentTarget",e.currentTarget); 
+    if(e.target === e.currentTarget){return this.props.handleCloseModal()}
+  }
   render() {
-    return <MyLoader/>
+    return (
+      <div className={style.overlay} onClick={this.handleClose} name="overlay">
+          <img name="picture"
+            src={this.props.imageModal}
+            alt={this.props.imageDescription}
+          />
+
+      </div>
+    );
   }
 }
