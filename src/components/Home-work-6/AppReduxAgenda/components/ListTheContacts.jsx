001c5487425470e3b2baacda45a80eb125b51001
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-07-02 06:55:38 +0300

    <AppReduxAgenda> editing action

--- src/components/Home-work-6/AppReduxAgenda/components/ListTheContacts.jsx
+++ src/components/Home-work-6/AppReduxAgenda/components/ListTheContacts.jsx
@@ -1,17 +1,54 @@
-import style from "./apReduxAgenda.module.scss";
-import { useSelector } from "react-redux";
+import { useState } from 'react';
+import style from './apReduxAgenda.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact, editContact } from '../redux/contactsSlice';
 
-const ListTheContacs = () => { 
-     const contacts = useSelector(state=>state.agendaRedux)
-        return (
-                <div className={style.listTheContacts}>
-                        <ul>
-                                {contacts.map(contact => <li key={contact.id}>
-                                        <span>{contact.name}  || {contact.number}</span>   
-                                </li>) }
-                        </ul>
-                </div>
-        )
-}
+const ListTheContacs = () => {
+  const [isEdit, setIsEdit] = useState(false);
 
-export default ListTheContacs
+ 
+  const contacts = useSelector(state => state.agendaRedux);
+  const dispatch = useDispatch();
+        const handleUpdateContact = (event,contactId) => {
+                event.preventDefault();
+          const form = event.currentTarget.elements;
+                const newName=form.newName.value.trim();
+                const newPhone = form.newPhone.value.trim();
+                console.log(newName,newPhone)
+                 dispatch(editContact({ id: contactId, number: newPhone, name: newName }));
+                setIsEdit(false)   
+  };
+  return (
+    <div className={style.listTheContacts}>
+      <ul>
+        {contacts.map((contact, index) => (
+                <li key={contact.id}>
+                        {!isEdit && <button onClick={() => setIsEdit(true)}  style={{marginRight:"10px"}}>Edit</button>}
+ {isEdit &&  <form onSubmit={e=>handleUpdateContact(e,contact.id)}>
+              <button>Save</button>
+              <input type="text" name="newName" placeholder="Adjust name..." />
+              <input
+                type="text"
+                name="newPhone"
+                placeholder="Adjust phone number..."
+              />
+            </form>}
+
+            <span>
+              {index + 1}. {contact.name} || {contact.number}
+            </span>
+            <button
+              type="button"
+              style={{ marginLeft: '15px' }}
+              onClick={() => dispatch(deleteContact(contact.id))}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListTheContacs;
--- src/components/Home-work-6/AppReduxAgenda/redux/contactsSlice.jsx
+++ src/components/Home-work-6/AppReduxAgenda/redux/contactsSlice.jsx
@@ -1,19 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit"
-localStorage.setItem("reduxContacts", JSON.stringify([]))
+
 const contactsLocal = localStorage.getItem("reduxContacts")
 
 const contactsSlice = createSlice({
         name: "contacts",
-        initialState: JSON.parse(contactsLocal),
+        initialState: JSON.parse(contactsLocal)||[],
         reducers: {
-                addNewContact: (state, action) => { state.push(action.payload);  localStorage.setItem("reduxContacts",JSON.stringify(state))}
+                addNewContact: (state, action) => { state.push(action.payload); localStorage.setItem("reduxContacts", JSON.stringify(state)) },
+                deleteContact: (state, action) => { const newState = state.filter(s => s.id !== action.payload); localStorage.setItem("reduxContacts", JSON.stringify(newState)); return newState },
+                editContact: (state, action) => {
+                        let updatedTask = state.find(s => s.id === action.payload.id);
+                        if (updatedTask) {
+                                updatedTask = { id: action.payload.id, number: action.payload.number, name: action.payload.name }
+                        }
+                        console.log("updatedTask",updatedTask)
+                        localStorage.setItem("reduxContacts", JSON.stringify(state))
+                }
         }
 })
 
-export const { addNewContact } = contactsSlice.actions;
+export const { addNewContact,deleteContact,editContact } = contactsSlice.actions;
 export const contactsSliceReducer = contactsSlice.reducer;
-
-
 
 
 
